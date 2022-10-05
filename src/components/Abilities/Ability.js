835ec2034189bb8e75c59e557e878462b38f7f95
Riley Sherlock <riley.sherlock@mts.net>
2022-10-05 17:57:32 -0500

    style skills

--- src/components/Abilities/Ability.js
+++ src/components/Abilities/Ability.js
@@ -3,15 +3,11 @@ import '../../styles/Ability.css';
 import Skill from './Skill';
 
 export default function Ability(props) {
-  let modifier = Math.floor((props.score - 10) / 2);
-
   return (
     <div className="ability">
       <div className="ability_values">
-        <div className="ability_score">{props.score}</div>
-        <div className="ability_modifier">{`${
-          modifier >= 0 ? '+' : '-'
-        }${modifier}`}</div>
+        <div className="ability_score"></div>
+        <div className="ability_modifier"></div>
         <div className="ability_name">{props.name.toUpperCase()}</div>
       </div>
       <div className="skills">
