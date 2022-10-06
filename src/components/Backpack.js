abc85c92517b3d07ac856dc99aa6d478d902149b
Riley Sherlock <riley.sherlock@mts.net>
2022-10-05 19:01:35 -0500

    add backpack component

---
+++ src/components/Backpack.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/Backpack.css';
+
+export default function Backpack(props) {
+  return (
+    <div className="backpack">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+}
--- src/components/Sheet.js
+++ src/components/Sheet.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Abilities from './Abilities/Abilities';
+import Backpack from './Backpack';
 import ChannelDivinity from './ChannelDivinity';
 import Defense from './Defense';
 import Healer from './Healer';
@@ -54,6 +55,7 @@ export default function Sheet(props) {
           body={`When you use your action to set it, this trap forms a saw-toothed steel ring that snaps shut when a creature steps on a pressure plate in the center. The trap is affixed by a heavy chain to an immobile object, such as a tree or a spike driven into the ground. A creature that steps on the plate must succeed on a DC 13 Dexterity saving throw or take 1d4 piercing damage and stop moving. Thereafter, until the creature breaks free of the trap, its movement is limited by the length of the chain (typically 3 feet long). A creature can use its action to make a DC 13 Strength check, freeing itself or another creature within its reach on a success. Each failed check deals 1 piercing damage to the trapped creature.`}
         />
       </div>
+      <Backpack />
     </div>
   );
 }
